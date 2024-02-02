26136fe53502d11eeae69c508eb04f675c7ad6e0
MehdiBenSalha <mehdi.bensalha@insat.u-carthage.tn>
2024-02-02 17:09:48 +0100

    Second Commit

---
+++ sum.py
@@ -0,0 +1,10 @@
+#sum function
+def sum(a, b):
+    return a + b
+
+
+x=input("Enter the first number: ")
+y=input("Enter the second number: ")
+
+print("The sum of the two numbers is: ", sum(int(x),int(y)))
+
